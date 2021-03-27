03c07bf72a793cabaee65342f59e16da747ce1d7
Javier Rubio <jrubio05@ucm.es>
2021-03-27 19:33:07 +0100

    ButtonCtrl ahora hace actuar a la entidad tras ser esta clickada

--- Proyecto.02/project/src/ButtonCtrl.h
+++ Proyecto.02/project/src/ButtonCtrl.h
@@ -11,9 +11,20 @@
 class ButtonCtrl : public Component
 {
     public:
-        ButtonCtrl(Game* g) :
-            Component(ecs::ButtonCtrl), ih_(nullptr), tr_(nullptr), speed_(10.0), g_(g) {};
-        virtual ~ButtonCtrl() {
+        ButtonCtrl(Game* g) : Component(ecs::ButtonCtrl),
+            ih_(nullptr),
+            tr_(nullptr),
+            speed_(10.0),
+            g_(g),
+            buttonRect_(nullptr),
+            mousePointer_(nullptr)
+        {
+
+        };
+        
+        virtual ~ButtonCtrl(){
+            delete buttonRect_; buttonRect_ = nullptr;
+            delete mousePointer_; mousePointer_ = nullptr;
         }
 
         inline void setSpeed(float speed) {
@@ -23,28 +34,26 @@ class ButtonCtrl : public Component
         void init() override {
             tr_ = GETCMP1_(Transform);
             assert(tr_ != nullptr);
+
             ih_ = InputHandler::instance();
+
+            Vector2D pos = tr_->getPos();
+            buttonRect_ = new SDL_Rect{ (int)pos.getX(), (int)pos.getY(), (int)tr_->getW(), (int)tr_->getH() };
         }
 
         void update() override {
             game_->getWindowHeight();
-            if (ih_->keyDownEvent()) {
-                if (ih_->isKeyDown(SDL_SCANCODE_UP)) {
-                    //tr_->setVelY(-speed_); //
-                    static_cast<Button*>(entity_)->click(g_);
-                }
-                else if (ih_->isKeyDown(SDL_SCANCODE_DOWN)) {
-                    tr_->setVelY(speed_); //
-                }
-                else if (ih_->isKeyDown(SDL_SCANCODE_LEFT)) {
-                    tr_->setVelX(-speed_); //
-                }
-                else if (ih_->isKeyDown(SDL_SCANCODE_RIGHT)) {
-                    tr_->setVelX(speed_); //
-                }
-                else if (ih_->isKeyDown(SDL_SCANCODE_SPACE)) {
-                    tr_->setVelY(0.0f);
-                    tr_->setVelX(0.0f);
+            if (ih_->mouseButtonEvent()) {
+                if (ih_->getMouseButtonState((InputHandler::MOUSEBUTTON)0)) {
+
+                    delete mousePointer_;
+                    Vector2D mouse = ih_->getMousePos();
+                    mousePointer_ = new SDL_Point{ (int)mouse.getX(), (int)mouse.getY() };
+
+                    if (SDL_PointInRect(mousePointer_, buttonRect_)) {
+                        //tr_->setVelY(-speed_); //
+                        static_cast<Button*>(entity_)->click(g_);
+                    }
                 }
             }
         }
@@ -54,8 +63,10 @@ class ButtonCtrl : public Component
         Game* g_;
         Transform* tr_;
         float speed_;
-    };
 
+        SDL_Rect* buttonRect_;
+        SDL_Point* mousePointer_ = nullptr;
+    };
 
 
 //Interfaz* i = new interfaz();
