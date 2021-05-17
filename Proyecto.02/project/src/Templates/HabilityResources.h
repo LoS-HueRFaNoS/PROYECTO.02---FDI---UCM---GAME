20e552dd1f05150d0dd2a8a7110e74e688e55344
pablos16 <pablos16@ucm.es>
2021-05-17 15:47:12 +0200

    arregladas las descripciones de varias habilidades y correjido el tipo de daño
    he arreglado el tipo de daño y la descripcion de las skills BloodyStrike y RockPillar y he corregido la descripcion de Whirpool

--- Proyecto.02/project/src/Templates/HabilityResources.h
+++ Proyecto.02/project/src/Templates/HabilityResources.h
@@ -168,10 +168,11 @@ public:
 		level = 0;
 		_mana = 0;
 		_name = "BloodyStrike";
-		_description = "Esto es un ejemplo, hace 1d5 de daño";
+		//_description = "Esto es un ejemplo, hace 1d5 de daño";
+		_description = "Ataque que hace 1d5 de daño a un enemigo y aplica sangrado";
 
 		_id = habID::BLOODYSTRIKE;
-		_damageType = damTy::ICE;
+		_damageType = damTy::SLASH;
 		_habilityType = habTy::ATTACK;
 		_mod = ms::INT;
 		_obj = objTy::SINGLEENEMY;
@@ -300,10 +301,10 @@ public:
 		level = 3;
 		_mana = 3;
 		_name = "Rock Pillar";
-		_description = "Mejorando la estructura, a hace 1d8 a un enemigo frente a salvacion DEX (reduce CON)";
+		_description = "Mejorando la estructura, hace 1d8 a un enemigo frente a salvacion DEX (reduce CON)";
 
 		_id = habID::ROCKPILLAR;
-		_damageType = damTy::WIND;
+		_damageType = damTy::EARTH;
 		_habilityType = habTy::ATTACK;
 		_mod = ms::INT;
 		_obj = objTy::SINGLEENEMY;
@@ -425,7 +426,7 @@ public:
 		level = 1;
 		_mana = 2;
 		_name = "Whirlpool";
-		_description = "Mas agua, a hace 1d8 a un enemigo frente a salvacion DEX (y reduce DEX)";
+		_description = "Mas agua, a hace 1d8 a un enemigo frente a salvacion DEX (y reduce STR)";
 
 		_id = habID::WHIRLPOOL;
 		_damageType = damTy::WATER;
