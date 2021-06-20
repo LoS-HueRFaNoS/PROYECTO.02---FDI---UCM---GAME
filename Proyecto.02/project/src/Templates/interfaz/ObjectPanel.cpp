a897b1f9cbd631f18758ce968b902634d6b56cb7
Pablo Cubells <pablocub@ucm.es>
2021-06-20 13:31:11 +0200

    Clase: ObjectPanel

---
+++ Proyecto.02/project/src/Templates/interfaz/ObjectPanel.cpp
@@ -0,0 +1,19 @@
+#include "ObjectPanel.h"
+
+void ObjectPanel::Init(SDL_Panel pan)
+{
+	marco_ = RECT(
+		pan.x,
+		pan.y,
+		pan.w,
+		pan.h
+	);
+
+	topElement_ = RECT(pan.fcx, pan.fcy, pan.cw, pan.ch);
+	bottomElement_ = RECT(pan.fcx, pan.lcy, pan.cw, pan.ch);
+	
+	tuppleLimits_ = Tupple(pan.fcy - 1, pan.lcy + 1);
+	tuppleMargins_ = Tupple(pan.mw, pan.mh);
+	tuppleEspaces_ = Tupple(pan.ew, pan.eh); // padding
+	tuppleBorders_ = Tupple(pan.bw, pan.bh); // borders
+}
